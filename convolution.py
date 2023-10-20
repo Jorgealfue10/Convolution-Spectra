a59a790522f92a2e00840d4e4c59bcdc6b86ef81
Jorgealfue10 <jorgealonsodelafuente@gmail.com>
2023-10-20 16:56:00 +0200

    Added Rot States

--- convolution.py
+++ convolution.py
@@ -10,15 +10,16 @@ import sys
 
 
 nfile = int(sys.argv[1])
+rots = sys.argv[-4]
 labcond = sys.argv[-3]
 amp = float(sys.argv[-2])
 png_name = sys.argv[-1]
 
-r=5
+r=6
 
 if len(sys.argv[:]) > r+nfile:
-    globalmin = float(sys.argv[-5])
-    globalmax = float(sys.argv[-4])
+    globalmin = float(sys.argv[-6])
+    globalmax = float(sys.argv[-5])
 
 
 
@@ -96,9 +97,18 @@ for m in range(nfile):
     if len(sys.argv[:]) > r+nfile:
         ax1.set_xlim(globalmin,globalmax)
 
+    if rots == "yes":
+        ax1.vlines(data[:,2],0,data[:,4])
+        for i in range(len(data[:,0])):
+            if data[i,4] > 0.1e-20: #np.max(data[i,4])/100:
+                if data[i,0] != data[i,1]:
+                   ax1.annotate(str(data[i,1])+r"->"+str(data[i,0]),xy=(data[i,2]-8,data[i,4]+0.5e-21),rotation=90,fontsize=15)
+                if data[i,0] == data[i,1] and data[i,1] == 0.5:
+                   ax1.annotate(str(data[i,1])+r"->"+str(data[i,0]),xy=(data[i,2]-8,data[i,4]+0.5e-21),rotation=90,fontsize=15)
+
 ax1.tick_params(axis='both', which='major', labelsize=30)
 ax1.set_xlabel(r" $\nu$ ($cm^{-1}$)",fontsize=30)
-ax1.set_ylabel(r" I (a.u.)",fontsize=30)
+ax1.set_ylabel(r" I (cm/mol)",fontsize=30)
 
 ax1.legend(fontsize=30,frameon=False)
 
